6fcbcf87cc0c591229865e1ce4023b1fe13d9058
tobi <tobi_h@outlook.com>
2026-06-04 23:50:16 +0000

    jump host e2e

--- src/pve_cloud_backup/daemon/brctl.py
+++ src/pve_cloud_backup/daemon/brctl.py
@@ -17,7 +17,7 @@ from kubernetes.client import (V1ConfigMapVolumeSource, V1Container, V1EnvVar,
                                V1Volume, V1VolumeMount)
 from kubernetes.config.kube_config import KubeConfigLoader
 from pve_cloud.cli.pvclu import (get_cloud_domain, get_cluster_vars,
-                                 get_ssh_master_kubeconfig)
+                                 get_ssh_master_kubeconfig, get_ssh_remote_master_kubeconfig)
 from pve_cloud.lib.inventory import get_online_pve_host
 from pve_cloud_backup._version import __version__ as bkp_version
 
@@ -160,17 +160,25 @@ async def launch_restore_job(args):
 
     # fetch the kubeconfig of the cluster we want to launch the restore job in
     online_pve_host, jump_host = get_online_pve_host(kubespray_inv["target_pve"])
-    if jump_host:
-        raise NotImplementedError(
-            "Jump host functionality not yet implemented for backup restore!"
-        )
 
+    external_cp_defined = "extra_control_plane_sans" in kubespray_inv and kubespray_inv["extra_control_plane_sans"]
+
+    if jump_host and not external_cp_defined :
+        raise NotImplementedError(
+            "Jump host functionality requires external san to be set for the kubernetes cluster!"
+        )
+    
     cluster_vars = get_cluster_vars(online_pve_host)
     cloud_domain = get_cloud_domain(kubespray_inv["target_pve"])
 
-    kubeconfig_dict = yaml.safe_load(
-        get_ssh_master_kubeconfig(cluster_vars, kubespray_inv["stack_name"])
-    )
+    if jump_host:
+       kubeconfig_dict = yaml.safe_load(
+            get_ssh_remote_master_kubeconfig(cluster_vars, kubespray_inv["stack_name"], kubespray_inv["extra_control_plane_sans"][0], jump_host, online_pve_host)
+        )
+    else:
+        kubeconfig_dict = yaml.safe_load(
+            get_ssh_master_kubeconfig(cluster_vars, kubespray_inv["stack_name"])
+        )
 
     # init kube client for launching the restore job
     loader = KubeConfigLoader(config_dict=kubeconfig_dict)
